1d0b2a247cb2feb8fd4b199e7d6567670c696cea
ai-null <ainul.y9b@gmail.com>
2025-01-01 20:01:08 +0700

    add getFinalState

---
+++ easy/getFinalState.cpp
@@ -0,0 +1,13 @@
+#include <utility>
+#include <vector>
+#include <iostream>
+
+vector<pair<int, int>> getFinalState(vector<int>& nums, int k, int multiplier) {
+    while (k--)
+    {
+        auto num = min_element(nums.begin(), min.end());
+        *it *= multiplier;
+    }
+    
+    return nums;
+}
